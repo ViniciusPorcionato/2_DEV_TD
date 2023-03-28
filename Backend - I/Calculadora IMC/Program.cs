9a6b66012e2446b7c57ef30179cac93f36a74e26
Vinicius <viniciusporcionato2005@gmail.com>
2023-03-28 17:21:08 -0300

    Introdução ao C#

---
+++ Backend - I/Calculadora IMC/Program.cs
@@ -0,0 +1,16 @@
+﻿// Calculadora IMC
+
+// Crie um programa para calcular o IMC
+
+// entrada
+
+string nome = "Vinicius";
+float peso = 89.9f;
+float altura = 1.80f;
+
+// processamento 
+
+float imc = peso / (altura * altura );
+
+Console.WriteLine($"O IMC  do " + nome + " é de : " +Math.Round(imc,2));
+
---
+++ Backend - I/Introdução ao C#/Program.cs
@@ -0,0 +1,113 @@
+﻿// variáveis
+
+// declarando uma variável
+
+// tipo nome = valor
+string nome = "Vinicius";
+const int idade = 17;
+Console.WriteLine($"A idade do " + nome + " é " + idade + " anos ");
+
+// tipos de dados
+int quantidade =10;
+double preco = 4.99D;
+float altura = 1.80f;
+bool careca = true;
+string texto = "Hello, World!";
+char letra = 'V';
+
+
+
+
+
+// Operadorer de atribuição
+
+int age = 45;
+
+// Operadores aritmético
+
+// soma
+Console.WriteLine(4 + 4);
+Console.WriteLine(4 + "4");
+
+// subtração
+Console.WriteLine(10 - 8);
+
+// multiplicação
+Console.WriteLine(4 * 9);
+
+// divisão
+Console.WriteLine(10 / 2);
+
+// modular
+Console.WriteLine(190 % 2);
+
+
+
+
+
+// operadores de comparação - resposta true or false
+
+// igual a 
+Console.WriteLine(5 == 5);
+
+// maior ou igual
+Console.WriteLine(6 >= 2);
+
+// menor ou igual 
+Console.WriteLine(8 <= 12);
+
+// maior que 
+Console.WriteLine(10 > 6);
+
+// menor que 
+Console.WriteLine(10 < 20);
+
+// diferente que 
+Console.WriteLine( 9 != 69);
+
+
+
+
+
+
+
+
+// operadores lógicos e tabela verdade
+
+// && : e
+
+//|| : ou
+
+// ! : negação
+
+Console.WriteLine(5 == 5 && 8 == 8); //true && true = true
+
+Console.WriteLine(8 == 4 && 9 == 9); //false && true = false
+
+Console.WriteLine(8 == 8 && 9 == 6); //true && false = false
+
+Console.WriteLine(8 == 2 && 9 == 6); //false && false = false
+
+
+
+Console.WriteLine( 2 == 2 || 3 == 3); // true || true = true
+
+Console.WriteLine( 2 == 2 || 5 == 3); // true || false = true
+
+Console.WriteLine( 2 == 6 || 3 == 3); // false || true = true
+
+Console.WriteLine( 2 == 8 || 3 == 9); // false || false = false
+
+
+
+
+
+Console.WriteLine(!( 2 == 2 || 3 == 3)); // true || true = false
+
+Console.WriteLine(!( 2 == 7 || 5 == 3)); // false || false = true
+
+
+
+
+
+
