5700a397f9bb5e541d818aa8d5781d6487a21bf7
Vinicius <viniciusporcionato2005@gmail.com>
2023-04-05 17:20:01 -0300

    Introdução a for loop

--- Backend - I/Estrutura de Repetição/Program.cs
+++ Backend - I/Estrutura de Repetição/Program.cs
@@ -3,9 +3,18 @@
 
 int x = 1;
 
-// laço de repetição
+// laço de repetição while
 while (x <= 10) //enquanto x for menor ou igual a 10
 {
    Console.WriteLine(x);// imprima o valor de x
     x++; //depois incrementa +1 ao valor de x
+}
+
+
+
+// laço de repetição for
+for (int i = 500; i >= 0; i-= 5)
+{
+    Console.WriteLine(i);
+    
 }
