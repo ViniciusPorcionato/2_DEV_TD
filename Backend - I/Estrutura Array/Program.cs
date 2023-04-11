d82cf1601ab0c0377308ac025a52852a802785f5
Vinicius <viniciusporcionato2005@gmail.com>
2023-04-11 14:23:26 -0300

    Introdução ao Array

---
+++ Backend - I/Estrutura Array/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] carros = new string[3];
+
+Console.WriteLine($"Digite o nome do carro :");
+carros[0] = Console.ReadLine();
+
+Console.WriteLine($"Digite o nome do carro :");
+carros[1] = Console.ReadLine();
+
+Console.WriteLine($"Digite o nome do carro :");
+carros[2] = Console.ReadLine();
+
+
+Console.WriteLine($"Primeiro carro da lista : {carros[0]}");
+Console.WriteLine($"Primeiro carro da lista : {carros[1]}");
+Console.WriteLine($"Primeiro carro da lista : {carros[2]}");
