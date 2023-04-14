3f4c1409538dda84b0ac412ede7ffd15af5475e3
Vinicius <viniciusporcionato2005@gmail.com>
2023-04-14 10:15:10 -0300

    Continuação dos exercícios de fixação

---
+++ Backend - I/Exercícos 5/Exercício achou ou não o nome/Program.cs
@@ -0,0 +1,33 @@
+﻿// Escreva um algoritmo que permita a leitura dos nomes de 10 pessoas e armazene os nomes
+// lidos em um vetor. Após isto, o algoritmo deve permitir a leitura de mais 1 nome qualquer de
+// pessoa (para efetuar uma busca) e depois escrever a mensagem ACHEI, se o nome estiver
+// entre os 10 nomes lidos anteriormente (guardados no vetor), ou NÃO ACHEI caso contrário.
+
+string[] nomes = new string[10];
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Informe o {i + 1}º nome:");
+    nomes[i] = Console.ReadLine().ToLower();
+      
+}
+
+Console.Write($"Informe o nome que você deseja procurar: ");
+
+string nomesProcurado = Console.ReadLine().ToLower();
+
+
+for (int i = 0; i < 10; i++)
+{
+    if(nomes[i].ToLower() == nomesProcurado.ToLower())
+{
+    Console.WriteLine($"Nome encontrado na posição {i + 1}º !");
+    return;
+    
+}
+else
+{
+    Console.WriteLine($"Nome não encontrado !");
+    
+}
+}
