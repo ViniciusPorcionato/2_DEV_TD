8cf5b6b52dbb4a7c1f95e22583939c7da153942d
Vinicius <viniciusporcionato2005@gmail.com>
2023-05-02 15:11:04 -0300

    Polimorfismo POO

---
+++ Backend - I/Programação Orientada ao Objeto (POO)/Polimorfismo POO/Credito.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Polimorfismo_POO
+{
+    public class Credito : Cartao
+    {
+        //Polimorfismo : Sobrescrita (override)
+        public override void Pagar()
+        {
+            //verificar se o limite atende 
+            //quantidade de parcelas
+            //a depender da quantidade de parcelas, temos um cálculo do valor a ser pago
+            //exibir o valor
+        }
+
+        //Polimorfismo : Sobrecarga (overload)
+        public float DescontoCredito(float valor)
+        {
+            return (valor * 0.9f);
+        }
+
+        public float DescontoCredito(float valor, string bandeira)
+        {
+            valor = (45.99f * 0.90f);
+            bandeira = "Master";
+            Console.WriteLine($"{bandeira}, {valor}");            
+            return valor; 
+        }
+    }
+}
---
+++ Backend - I/Programação Orientada ao Objeto (POO)/Polimorfismo POO/Debito.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Polimorfismo_POO
+{
+    public class Debito
+    {
+        //Polimorfismo : Sobrescrita
+        public override void Pagar()
+        {
+            //verificar se o saldo da conta é suficiente 
+            //caso não seja, informar(exibir uma mensagem)
+            //caso seja suficiente, exiba o valor a ser pago 
+        }
+    }
+}
